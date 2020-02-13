13dd5be67f62fdb711721defa6b766bf839c9756
rleibmann <ruben.leibmann@gmx.de>
2020-02-13 16:00:21 +0100

    Added UsersTest.java

--- src/test/java/stocks/repo/UsersTest.java
+++ src/test/java/stocks/repo/UsersTest.java
@@ -1,48 +1,49 @@
 package stocks.repo;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import stocks.entities.User;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsersTest {
 
+    @BeforeAll
+    static void setUp() {
+        Users.add(new User("user"));
+    }
+
     @Test
     void get() {
+        assertEquals(new User("user"), Users.get("user"));
     }
 
     @Test
     void getAll() {
-    }
-
-    @Test
-    void add() {
+        List<User> users = Users.getAll();
+        assertTrue(users.contains(new User("user")));
     }
 
     @Test
     void contains() {
+        assertTrue(Users.contains(new User("user")));
     }
 
     @Test
     void testContains() {
+        assertTrue(Users.contains(new User("test")));
     }
 
     @Test
     void indexOf() {
+        assertEquals(0, Users.indexOf(new User("user")));
     }
 
     @Test
     void isEmpty() {
-    }
-
-    @Test
-    void save() {
-    }
-
-    @Test
-    void load() {
-    }
-
-    @Test
-    void updatePortfolios() {
+        assertFalse(Users.isEmpty());
     }
 }
